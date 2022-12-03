0e739c6d80b511064c4a96f30bd832439fb2caa2
Kolodii Kvitoslava <92575017+kvitK1@users.noreply.github.com>
2022-12-03 22:43:44 +0200

    oops

---
+++ lab10/src/main/java/task2/TwitterClient.java
@@ -0,0 +1,23 @@
+package task2;
+
+import java.time.LocalDate;
+
+public class TwitterClient implements Client {
+    public TwitterClient(TwitterUser ukraine) {
+    }
+
+    @Override
+    public String getEmail() {
+        return null;
+    }
+
+    @Override
+    public String getCountry() {
+        return null;
+    }
+
+    @Override
+    public LocalDate getLastActiveTime() {
+        return null;
+    }
+}
